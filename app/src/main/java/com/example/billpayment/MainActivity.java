6ff19cf6eac3cdab4caec88cf1ef3079734e43a7
MaryamHasani <Maryamhasanibusiness@gmail.com>
2023-06-02 11:53:08 +0330

    complete main activity design

--- app/src/main/java/com/example/billpayment/MainActivity.java
+++ app/src/main/java/com/example/billpayment/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.billpayment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class MainActivity extends AppCompatActivity {
 
+    MaterialCardView bill,charge;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bill = findViewById(R.id.fixed_line_bill);
+        charge = findViewById(R.id.buy_charge);
+
+        bill.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, FixedLineBillActivity.class);
+            startActivity(intent);
+        });
+
+        charge.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, BuyChargeActivity.class);
+            startActivity(intent);
+        });
     }
 }
