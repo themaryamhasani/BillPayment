1e78dca4708fb34ebc97f9b8c6d8c044ef002116
MaryamHasani <Maryamhasanibusiness@gmail.com>
2023-06-02 10:48:54 +0330

    add fixedLineBill,buyCharge activities

---
+++ app/src/main/java/com/example/billpayment/FixedLineBillActivity.java
@@ -0,0 +1,14 @@
+package com.example.billpayment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FixedLineBillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fixed_line_bill);
+    }
+}
