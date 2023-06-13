08947c20ceb2aca66686588d6d657ffb5eb70ff2
Saba <Saba.bayatloo513@gmail.com>
2023-06-13 23:01:02 +0330

    complete fixed line activity

--- app/src/main/java/com/example/billpayment/FixedLineBillActivity.java
+++ app/src/main/java/com/example/billpayment/FixedLineBillActivity.java
@@ -1,10 +1,21 @@
 package com.example.billpayment;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import static android.app.PendingIntent.getActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -18,25 +29,44 @@ import okhttp3.Response;
 
 public class FixedLineBillActivity extends AppCompatActivity {
 
+    Button button;
+    EditText fixedLine;
+    TextView billId,midPayId,finalPayId,midTerm,finalTerm;
+    ConstraintLayout layout;
     public String postUrl = "https://charge.sep.ir/Inquiry/FixedLineBillInquiry";
     OkHttpClient client = new OkHttpClient();
-    String bill;
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fixed_line_bill);
-        try {
-            callAPI("02122129043");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+
+        button = findViewById(R.id.button);
+        fixedLine = findViewById(R.id.editText);
+        layout = findViewById(R.id.layout);
+        billId = findViewById(R.id.bill_id);
+        midPayId = findViewById(R.id.mid_payment_id);
+        finalPayId = findViewById(R.id.final_payment_id);
+        midTerm = findViewById(R.id.midTerm);
+        finalTerm = findViewById(R.id.finalTerm);
+
+        button.setOnClickListener(view -> {
+
+            try {
+                callAPI("{\n" +
+                        "    \"FixedLineNumber\": \"" + fixedLine.getText() + "\"\n" +
+                        "}");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
-    void callAPI(String number) throws IOException {
 
-        RequestBody body = RequestBody.create(JSON, number);
+    void callAPI(String data) throws IOException {
+
+        RequestBody body = RequestBody.create(JSON, data);
         Request request = new Request.Builder()
                 .url(postUrl)
                 .post(body)
@@ -46,11 +76,36 @@ public class FixedLineBillActivity extends AppCompatActivity {
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
                 call.cancel();
             }
+
             @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                assert response.body() != null;
-                bill = response.body().string();
-                runOnUiThread(() -> Toast.makeText(FixedLineBillActivity.this, bill, Toast.LENGTH_SHORT).show());
+            public void onResponse(@NonNull Call call, @NonNull Response response) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            assert response.body() != null;
+                            String jsonData = response.body().string();
+                            JSONObject object = new JSONObject(jsonData);
+                            if (object.getString("code").equals("-16")) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(FixedLineBillActivity.this);
+                                builder.setMessage("هیچ قبض قابل پرداختی برای این تلفن ثابت استعلام شده ثبت نشده است")
+                                        .setTitle("قبض یافت نشد!")
+                                        .setCancelable(true);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            } else {
+                                layout.setVisibility(View.VISIBLE);
+                                finalTerm.setText(object.getJSONObject("data").getJSONObject("FinalTerm").getString("Amount"));
+                                midTerm.setText(object.getJSONObject("data").getJSONObject("MidTerm").getString("Amount"));
+                                finalPayId.setText(object.getJSONObject("data").getJSONObject("FinalTerm").getString("PaymentID"));
+                                midPayId.setText(object.getJSONObject("data").getJSONObject("MidTerm").getString("PaymentID"));
+                               billId.setText(object.getJSONObject("data").getJSONObject("FinalTerm").getString("BillID"));
+                            }
+                        } catch (JSONException | IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
             }
         });
     }
